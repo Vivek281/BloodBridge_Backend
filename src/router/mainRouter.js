2cd86a87c21da0b893c84ccb332cfb77ce3f0362
Vivek281 <vivekjha2062@gmail.com>
2026-04-09 13:52:51 +0545

    Donation router added to main router

--- src/router/mainRouter.js
+++ src/router/mainRouter.js
@@ -1,8 +1,10 @@
 const authRouter = require("./authRouter");
 const mainRouter = require("express").Router();
-const bloodRequestRouter = require("./bloodRequestRoutrer")
+const bloodRequestRouter = require("./bloodRequestRoutrer");
+const donationRouter = require("./donationRouter");
 
 mainRouter.use("/auth",authRouter);
-mainRouter.use("/blood-request", bloodRequestRouter)
+mainRouter.use("/blood-request", bloodRequestRouter);
+mainRouter.use("/donation", donationRouter);
 
 module.exports = mainRouter
